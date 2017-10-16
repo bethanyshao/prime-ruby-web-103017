33bd2c74fccad1108fdec49eb9bebdf334c74bf2
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-16 21:45:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   if num == 0 || num == 1
     true
   end
-  nums_to_test = (2...num)
+  nums_to_test = (2...num).to_a
   nums_to_test.each do |num_to_test|
     if num % num_to_test == 0
       false
